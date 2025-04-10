9065600f3e927bb3aacb28a61d109d8dde7f3686
nxxx-oc <nna894031@gmail.com>
2025-04-10 12:57:38 +0900

    fix: CORS 문제 아마 해결

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
