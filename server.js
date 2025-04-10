9ecdf72adfa57c052dadfb64d2f8e13945e88a78
nxxx-oc <nna894031@gmail.com>
2025-04-10 12:54:08 +0900

    CORS 설정 추가

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors())
 
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
